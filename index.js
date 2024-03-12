3bc6f0fe38b4a3af8a3aef2ae740716078bc284a
Annania Plossl <nia.plossl@gmail.com>
2024-03-12 13:31:46 -0400

    completed the lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(array,match) {
+    return array.filter((element)=>{
+        return element.toUpperCase()===match.toUpperCase();
+    });
+}
+function fuzzyMatch(array,match) {
+    return array.filter((element)=>{
+        element=element.substr(0,match.length);
+        return element===match;
+    });
+}
+function matchName(array,match) {
+    return array.filter((element)=>{
+        return element.name===match;
+    });
+}
